8449188443050610eda7487b7ab9d949bba0c4d6
Lewis Goddard <lewisgoddard@eustasy.org>
2017-05-02 17:45:14 +0100

    Update JS CodeStyle

--- vanilla.downboy.auto.js
+++ vanilla.downboy.auto.js
@@ -5,52 +5,52 @@
  */
 
 (function (exports, augment, $) {
-	'use strict';
-	augment(Array, ['forEach']);
+	'use strict'
+	augment(Array, ['forEach'])
 
 	/**
-	 * This function is exactly the same as in 
+	 * This function is exactly the same as in
 	 * jQuery.downBoy
 	 */
 	exports.downBoy = function (footer) {
-		footer = footer || 'footer';
-		$(footer).css('position', '').css('bottom', '');
+		footer = footer || 'footer'
+		$(footer).css('position', '').css('bottom', '')
 		if ($(window).height() > $('body').height()) {
-			$(footer).css('position', 'absolute').css('bottom', '0');
+			$(footer).css('position', 'absolute').css('bottom', '0')
 		}
-	};
+	}
 
 	// When the page has loaded
 	$(window).on('DOMContentLoaded', function () {
-		var resizeTimeout;
-		downBoy(); // Run the function
+		var resizeTimeout
+		downBoy() // Run the function
 
 		// And every time the window is resized
 		$(window).on('resize', function () {
-			if (resizeTimeout) return;
+			if (resizeTimeout) return
 
 		// Will execute at a rate of 15fps
 			resizeTimeout = setTimeout(function() {
-				resizeTimeout = null;
-				downBoy(); // Run again
-		}, 66);
-		});
-	});
+				resizeTimeout = null
+				downBoy() // Run again
+		}, 66)
+		})
+	})
 
 })(window,
 /**
  * Augment Class with some of its prototype
  */
 function augment (Class, keys) {
-	'use strict';
-	var slice = Array.prototype.slice;
+	'use strict'
+	var slice = Array.prototype.slice
 	keys.forEach(function (key) {
-		var method = Class.prototype[key];
+		var method = Class.prototype[key]
 		Class[key] = function () {
-			var args = slice.call(arguments, 1);
-			method.apply(arguments[0], args);
-		};
-	});
+			var args = slice.call(arguments, 1)
+			method.apply(arguments[0], args)
+		}
+	})
 }, window.jQuery ||
 /**
  * Here comes a teeny-tiny jQuery shim (just because
@@ -60,50 +60,50 @@ function augment (Class, keys) {
  * included before this one.
  */
 function jQuery (sel, ctx) {
-	'use strict';
+	'use strict'
 	switch (typeof sel) {
 		case 'string':
-			ctx = ctx || document;
-			sel = ctx.querySelectorAll(sel);
+			ctx = ctx || document
+			sel = ctx.querySelectorAll(sel)
 			return {
 				css: function (key, val) {
 					Array.forEach(sel, function each (item) {
-						item.style[key] = val;
-					});
-					return this;
+						item.style[key] = val
+					})
+					return this
 				},
 				height: function () {
-					var style = getComputedStyle(sel[0]);
-					var val = style.getPropertyValue('height');
-					return parseFloat(val);
-				}
-			};
+					var style = getComputedStyle(sel[0])
+					var val = style.getPropertyValue('height')
+					return parseFloat(val)
+				},
+			}
 
 		case 'object':
 			if (sel == null) {
-				throw "Bad usage of $ - the first argument is null";
+				throw "Bad usage of $ - the first argument is null"
 			}
 
-			var html = document.documentElement;
+			var html = document.documentElement
 			return {
 				height: function () {
 					if (sel instanceof Window) {
-						return html.clientHeight;
+						return html.clientHeight
 					}
-					var style = getComputedStyle(sel[0]);
-					var val = style.getPropertyValue('height');
-					return parseFloat(val);
+					var style = getComputedStyle(sel[0])
+					var val = style.getPropertyValue('height')
+					return parseFloat(val)
 				},
 				on: function (event, handler) {
-					sel.addEventListener(event, handler);
-					return this;
-				}
+					sel.addEventListener(event, handler)
+					return this
+				},
 			}
 
 		case 'undefined':
-			throw "Bad usage of $ - nothing given as first argument";
+			throw "Bad usage of $ - nothing given as first argument"
 
 		default:
-			throw "$ doesn't currently work on a " + (typeof sel);
+			throw "$ doesn't currently work on a " + (typeof sel)
 	}
-});
+})
--- vanilla.downboy.js
+++ vanilla.downboy.js
@@ -5,35 +5,35 @@
  */
 
 (function (exports, augment, $) {
-	'use strict';
-	augment(Array, ['forEach']);
+	'use strict'
+	augment(Array, ['forEach'])
 
 	/**
-	 * This function is exactly the same as in 
+	 * This function is exactly the same as in
 	 * jQuery.downBoy
 	 */
 	exports.downBoy = function (footer) {
-		footer = footer || 'footer';
-		$(footer).css('position', '').css('bottom', '');
+		footer = footer || 'footer'
+		$(footer).css('position', '').css('bottom', '')
 		if ($(window).height() > $('body').height()) {
-			$(footer).css('position', 'absolute').css('bottom', '0');
+			$(footer).css('position', 'absolute').css('bottom', '0')
 		}
-	};
-	
+	}
+
 })(window,
 /**
  * Augment Class with some of its prototype
  */
 function augment (Class, keys) {
-	'use strict';
-	var slice = Array.prototype.slice;
+	'use strict'
+	var slice = Array.prototype.slice
 	keys.forEach(function (key) {
-		var method = Class.prototype[key];
+		var method = Class.prototype[key]
 		Class[key] = function () {
-			var args = slice.call(arguments, 1);
-			method.apply(arguments[0], args);
-		};
-	});
+			var args = slice.call(arguments, 1)
+			method.apply(arguments[0], args)
+		}
+	})
 }, window.jQuery ||
 /**
  * Here comes a teeny-tiny jQuery shim (just because
@@ -43,46 +43,46 @@ function augment (Class, keys) {
  * included before this one.
  */
 function jQuery (sel, ctx) {
-	'use strict';
+	'use strict'
 	switch (typeof sel) {
 		case 'string':
-			ctx = ctx || document;
-			sel = ctx.querySelectorAll(sel);
+			ctx = ctx || document
+			sel = ctx.querySelectorAll(sel)
 			return {
 				css: function (key, val) {
 					Array.forEach(sel, function each (item) {
-						item.style[key] = val;
-					});
-					return this;
+						item.style[key] = val
+					})
+					return this
 				},
 				height: function () {
-					var style = getComputedStyle(sel[0]);
-					var val = style.getPropertyValue('height');
-					return parseFloat(val);
-				}
-			};
+					var style = getComputedStyle(sel[0])
+					var val = style.getPropertyValue('height')
+					return parseFloat(val)
+				},
+			}
 
 		case 'object':
 			if (sel == null) {
-				throw "Bad usage of $ - the first argument is null";
+				throw "Bad usage of $ - the first argument is null"
 			}
 
-			var html = document.documentElement;
+			var html = document.documentElement
 			return {
 				height: function () {
 					if (sel instanceof Window) {
-						return html.clientHeight;
+						return html.clientHeight
 					}
-					var style = getComputedStyle(sel[0]);
-					var val = style.getPropertyValue('height');
-					return parseFloat(val);
-				}
+					var style = getComputedStyle(sel[0])
+					var val = style.getPropertyValue('height')
+					return parseFloat(val)
+				},
 			}
 
 		case 'undefined':
-			throw "Bad usage of $ - nothing given as first argument";
+			throw "Bad usage of $ - nothing given as first argument"
 
 		default:
-			throw "$ doesn't currently work on a " + (typeof sel);
+			throw "$ doesn't currently work on a " + (typeof sel)
 	}
-});
+})
